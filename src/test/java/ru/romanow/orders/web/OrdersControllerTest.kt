562ffe48dba0039cbca682fd3b41a2a6e9235a1f
aromanov <aromanov@inno.tech>
2021-09-04 00:18:35 +0300

    fix tests

--- src/test/java/ru/romanow/orders/web/OrdersControllerTest.kt
+++ src/test/java/ru/romanow/orders/web/OrdersControllerTest.kt
@@ -35,8 +35,9 @@ import java.util.*
         "ru.romanow.scc:warehouse:[1.0.0,2.0.0):stubs:8070",
         "ru.romanow.scc:delivery:[1.0.0,2.0.0):stubs:8090"
     ],
+    repositoryRoot = "https://romanow.jfrog.io/artifactory/scc-libs-release/",
     mappingsOutputFolder = "build/mappings",
-    stubsMode = StubRunnerProperties.StubsMode.LOCAL
+    stubsMode = StubRunnerProperties.StubsMode.REMOTE
 )
 @Transactional
 @AutoConfigureTestEntityManager
